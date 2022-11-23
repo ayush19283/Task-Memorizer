13268610c399455a3e139419489f771524f4d737
ayush <ayushsharma19283@gmail.com>
2022-11-23 20:00:08 +0530

    ui ready

---
+++ app/src/main/java/com/ayush/memorizer/task.kt
@@ -0,0 +1,22 @@
+package com.ayush.memorizer
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.blue)
+            window.navigationBarColor=this.resources.getColor(R.color.blue)
+
+        }
+
+    }
+}
