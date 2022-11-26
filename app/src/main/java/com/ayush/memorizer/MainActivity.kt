687694624211fce367a303894b2659f656ecf99c
ayush <ayushsharma19283@gmail.com>
2022-11-26 22:09:21 +0530

    ui updated

--- app/src/main/java/com/ayush/memorizer/MainActivity.kt
+++ app/src/main/java/com/ayush/memorizer/MainActivity.kt
@@ -8,14 +8,13 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_task)
         if (Build.VERSION.SDK_INT >= 21) {
             val window = this.window
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             window.statusBarColor = this.resources.getColor(R.color.blue)
             window.navigationBarColor=this.resources.getColor(R.color.blue)
-
         }
     }
 }
